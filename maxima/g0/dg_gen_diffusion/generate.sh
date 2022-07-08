c074fce0bb1a9ce4bd64175565e11fc9dcf84c75
Petr Cagas <pcagas@vt.edu>
2022-07-07 22:02:49 -0400

    Maxima scripts for generating general diffusion kernels.

--- maxima/g0/dg_gen_diffusion/generate.sh
+++ maxima/g0/dg_gen_diffusion/generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#maxima -r 'load("diff-surf-2x-C.mac");quit();'
+maxima -r 'load("diff-surf-2x-C.mac");quit();'
 maxima -r 'load("diff-vol-2x-C.mac");quit();'
+maxima -r 'load("diff-surf-3x-C.mac");quit();'
+maxima -r 'load("diff-vol-3x-C.mac");quit();'
