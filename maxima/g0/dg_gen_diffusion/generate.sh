ff22716121fce66681b8f0f6371abc4ec246bf5f
Petr Cagas <pcagas@vt.edu>
2022-07-08 17:22:32 -0400

    Fixing bug in 3D general diffusion scripts. It now fully works in 3D up to p=2

--- maxima/g0/dg_gen_diffusion/generate.sh
+++ maxima/g0/dg_gen_diffusion/generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+maxima -r 'load("diff-vol-2x-C.mac");quit();'
+maxima -r 'load("diff-vol-3x-C.mac");quit();'
+
 maxima -r 'load("diff-surfx-2x-C.mac");quit();'
 maxima -r 'load("diff-surfy-2x-C.mac");quit();'
 
-#maxima -r 'load("diff-surf-2x-C.mac");quit();'
-#maxima -r 'load("diff-vol-2x-C.mac");quit();'
-#maxima -r 'load("diff-surf-3x-C.mac");quit();'
-#maxima -r 'load("diff-vol-3x-C.mac");quit();'
+maxima -r 'load("diff-surfx-3x-C.mac");quit();'
+maxima -r 'load("diff-surfy-3x-C.mac");quit();'
+maxima -r 'load("diff-surfz-3x-C.mac");quit();'
