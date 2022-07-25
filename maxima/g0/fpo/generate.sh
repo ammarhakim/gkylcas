910093925606ab630651ff2b38fca1490ef60b5f
Petr Cagas <pcagas@vt.edu>
2022-07-25 18:47:27 -0400

    Modifying LBO drag kernels for FPO

--- maxima/g0/fpo/generate.sh
+++ maxima/g0/fpo/generate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-maxima -r 'load("ms-vlasov-header.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-header.mac");quit();'
+
+maxima -r 'load("ms-fpo-vlasov-drag-vol.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
+#maxima -r 'load("ms-fpo-vlasov-drag-boundary-surf.mac");quit();'
 
 maxima -r 'load("ms-fpo-vlasov-diff-vol.mac");quit();'
 maxima -r 'load("ms-fpo-vlasov-diff-surf.mac");quit();'
