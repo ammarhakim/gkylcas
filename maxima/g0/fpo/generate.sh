108a3a88015876e31767b308b5b6a9b278a7cacf
Petr Cagas <pcagas@vt.edu>
2022-11-15 12:13:17 -0500

    FPO D volume kernels added

--- maxima/g0/fpo/generate.sh
+++ maxima/g0/fpo/generate.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#maxima -r 'load("ms-fpo-vlasov-header.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-header.mac");quit();'
 
-#maxima -r 'load("ms-fpo-vlasov-drag-vol.mac");quit();'
-#maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
-#maxima -r 'load("ms-fpo-vlasov-drag-boundary-surf.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-drag-vol.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-drag-boundary-surf.mac");quit();'
 
-#maxima -r 'load("ms-fpo-vlasov-diff-vol.mac");quit();'
-#maxima -r 'load("ms-fpo-vlasov-diff-surf.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-diff-vol.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-diff-surf.mac");quit();'
+#maxima -r 'load("ms-fpo-vlasov-diff-boundary-surf.mac");quit();'
 
+maxima -r 'load("ms-fpo-vlasov-D-vol.mac");quit();'
 maxima -r 'load("ms-fpo-vlasov-D-surf.mac");quit();'
+#maxima -r 'load("ms-fpo-vlasov-D-boundary-surf.mac");quit();'
