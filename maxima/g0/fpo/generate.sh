0a8522a8a9146befc133e4428740eac2dce710fd
Petr Cagas <pcagas@vt.edu>
2022-11-15 10:09:46 -0500

    Adding scripts to calculate the g to D kernels

--- maxima/g0/fpo/generate.sh
+++ maxima/g0/fpo/generate.sh
@@ -3,8 +3,10 @@
 #maxima -r 'load("ms-fpo-vlasov-header.mac");quit();'
 
 #maxima -r 'load("ms-fpo-vlasov-drag-vol.mac");quit();'
-maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
+#maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
 #maxima -r 'load("ms-fpo-vlasov-drag-boundary-surf.mac");quit();'
 
 #maxima -r 'load("ms-fpo-vlasov-diff-vol.mac");quit();'
 #maxima -r 'load("ms-fpo-vlasov-diff-surf.mac");quit();'
+
+maxima -r 'load("ms-fpo-vlasov-D-surf.mac");quit();'
