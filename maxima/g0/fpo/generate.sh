4a7f4b9ca41bdb45d0ce906e4fd52f3b89ce751c
John Rodman <jrodman@vt.edu>
2025-02-08 16:41:25 -0500

    Update old generation script

--- maxima/g0/fpo/generate.sh
+++ maxima/g0/fpo/generate.sh
@@ -2,14 +2,19 @@
 
 maxima -r 'load("ms-fpo-vlasov-header.mac");quit();'
 
+maxima -r 'load("ms-fpo-drag-coeff.mac");quit();'
+maxima -r 'load("ms-fpo-diff-coeff.mac");quit();'
+maxima -r 'load("ms-fpo-diff-coeff-surf.mac");quit();'
+
+maxima -r 'load("ms-fpo-vlasov-coeff-correct.mac");quit();'
+
 maxima -r 'load("ms-fpo-vlasov-drag-vol.mac");quit();'
 maxima -r 'load("ms-fpo-vlasov-drag-surf.mac");quit();'
 maxima -r 'load("ms-fpo-vlasov-drag-boundary-surf.mac");quit();'
 
 maxima -r 'load("ms-fpo-vlasov-diff-vol.mac");quit();'
 maxima -r 'load("ms-fpo-vlasov-diff-surf.mac");quit();'
-#maxima -r 'load("ms-fpo-vlasov-diff-boundary-surf.mac");quit();'
+maxima -r 'load("ms-fpo-vlasov-diff-boundary-surf.mac");quit();'
 
-maxima -r 'load("ms-fpo-vlasov-D-vol.mac");quit();'
-maxima -r 'load("ms-fpo-vlasov-D-surf.mac");quit();'
-#maxima -r 'load("ms-fpo-vlasov-D-boundary-surf.mac");quit();'
+maxima -r 'load("ms-moments-vlasov-fpo.mac");quit();'
+maxima -r 'load("ms-moments-vlasov-fpo-header.mac");quit();'
