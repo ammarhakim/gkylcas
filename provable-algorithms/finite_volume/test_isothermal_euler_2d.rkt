06c0a27db36f37723f47b4b31f0953321a998316
JonathanGorard <jg865@cantab.ac.uk>
2025-11-04 18:12:38 -0500

    Added new code generator to the underlying finite volume algorithm code generation framework to handle 2D vector systems of coupled vector PDEs (Lax-Friedrichs fluxes, first-order splitting and reconstruction), and added a 2D variant of the isothermal Euler test case. No formal verification of the resulting algorithm, yet.

---
+++ provable-algorithms/finite_volume/test_isothermal_euler_2d.rkt
@@ -0,0 +1,99 @@
+#lang racket
+
+(require "code_generator_core.rkt")
+(require "code_generator_vector.rkt")
+(require "prover_core.rkt")
+(require "prover_vector.rkt")
+(provide (all-from-out "code_generator_core.rkt"))
+(provide (all-from-out "code_generator_vector.rkt"))
+
+;; Construct /code and /proofs output directories if they do not already exist.
+(cond
+  [(not (directory-exists? "code")) (make-directory "code")])
+(cond
+  [(not (directory-exists? "proofs")) (make-directory "proofs")])
+
+;; Define the 2D isothermal Euler equations (density, x-momentum and y-momentum components).
+(define pde-system-isothermal-euler-2d
+  (hash
+   'name "isothermal-euler-2d"
+   'cons-exprs (list
+                `rho
+                `mom_x
+                `mom_y)                                        ; conserved variables: density, x-momentum, y-momentum
+   'flux-exprs-x (list
+                  `mom_x
+                  `(+ (/ (* mom_x mom_x) rho) (* rho vt vt))
+                  `(* mom_y (/ mom_x rho)))                    ; x-flux vector
+   'flux-exprs-y (list
+                  `mom_y
+                  `(* mom_x (/ mom_y rho))
+                  `(+ (/ (* mom_y mom_y) rho) (* rho vt vt)))  ; y-flux vector
+   'max-speed-exprs-x (list
+                       `(abs (- (/ mom_x rho) vt))
+                       `(/ mom_x rho)
+                       `(abs (+ (/ mom_x rho) vt)))            ; local wave-speeds (x-direction)
+   'max-speed-exprs-y (list
+                       `(abs (- (/ mom_y rho) vt))
+                       `(/ mom_y rho)
+                       `(abs (+ (/ mom_y rho) vt)))            ; local wave-speeds (y-direction)
+   'parameters (list
+                `(define vt 1.0))                              ; thermal velocity: vt = 1.0
+   ))
+
+;; Define 2D simulation parameters.
+(define nx-2d 50)
+(define ny-2d 50)
+(define x0-2d 0.0)
+(define x1-2d 2.0)
+(define y0-2d 0.0)
+(define y1-2d 2.0)
+(define t-final-2d 0.2)
+(define cfl-2d 0.95)
+(define init-funcs-2d (list
+                       `(cond
+                          [(> y 1.0)
+                           (cond
+                             [(< x 1.0) 0.5323]
+                             [else 1.5])]
+                          [else
+                           (cond
+                             [(< x 1.0) 0.138]
+                             [else 0.5323])])
+                       `(cond
+                          [(> y 1.0)
+                           (cond
+                             [(< x 1.0) 1.206]
+                             [else 0.0])]
+                          [else
+                           (cond
+                             [(< x 1.0) 1.206]
+                             [else 0.0])])
+                       `(cond
+                          [(> y 1.0)
+                           (cond
+                             [(< x 1.0) 0.0]
+                             [else 0.0])]
+                          [else
+                           (cond
+                             [(< x 1.0) 1.206]
+                             [else 1.206])])))
+
+;; Synthesize the code for a Lax-Friedrichs solver for the 2D isothermal Euler equations.
+(define code-isothermal-euler-lax-2d
+  (generate-lax-friedrichs-vector3-2d pde-system-isothermal-euler-2d
+                                      #:nx nx-2d
+                                      #:ny ny-2d
+                                      #:x0 x0-2d
+                                      #:x1 x1-2d
+                                      #:y0 y0-2d
+                                      #:y1 y1-2d
+                                      #:t-final t-final-2d
+                                      #:cfl cfl-2d
+                                      #:init-funcs init-funcs-2d))
+
+;; Output the code to a file.
+(with-output-to-file "code/isothermal_euler_lax_2d.c"
+  #:exists 'replace
+  (lambda ()
+    (display code-isothermal-euler-lax-2d)))
