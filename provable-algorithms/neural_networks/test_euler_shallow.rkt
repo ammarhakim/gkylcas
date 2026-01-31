b7e1fc097be41d3cb4a2fc47a3a42465e77833e8
JonathanGorard <jg865@cantab.ac.uk>
2026-01-31 18:05:51 -0500

    Implemented neural network training infrastructure for 2D matrix systems of PDEs (using first-order operator splitting, with no second-order flux extrapolation yet). Updated the compressible Euler test to use this new infrastructure, with the full version of the 2D quadrants test. The results look pretty qualitatively bad, but I'm not sure yet whether that's due to a bug or just a byproduct of the diffusivity of the underlying solver.

--- provable-algorithms/neural_networks/test_euler_shallow.rkt
+++ provable-algorithms/neural_networks/test_euler_shallow.rkt
@@ -1,11 +1,14 @@
 #lang racket
 
 (require "code_generator_core_training.rkt")
-(require "code_generator_matrix_validation.rkt")
 (require "code_generator_matrix_training.rkt")
+(require "code_generator_matrix_training_2d.rkt")
+(require "code_generator_matrix_validation.rkt")
 (require "prover_matrix.rkt")
 (provide (all-from-out "code_generator_core_training.rkt"))
 (provide (all-from-out "code_generator_matrix_training.rkt"))
+(provide (all-from-out "code_generator_matrix_training_2d.rkt"))
+(provide (all-from-out "code_generator_matrix_validation.rkt"))
 
 ;; Construct /code and /proofs output directories if they do not already exist.
 (cond
@@ -57,7 +60,7 @@
   (hash
    'max-trains 10000    ; maximum number of training steps: 10000
    'width 64            ; number of neurons in each layer: 64
-   'depth 8             ; total number of layers: 6
+   'depth 6             ; total number of layers: 6
    ))
 
 ;; Synthesize the code to train a Lax-Friedrichs surrogate solver for the 1D Euler equations using a shallow neural network.
@@ -177,3 +180,114 @@
   #:exists 'replace
   (lambda ()
     (display code-euler-minmod-validate)))
+
+;; Define the 2D Euler equations (density, x-momentum and y-momentum, and total energy components).
+(define pde-system-euler-2d
+  (hash
+   'name "euler-2d"
+   'cons-exprs (list
+                `rho
+                `mom_x
+                `mom_y
+                `energy)                                 ; conserved variables: density, x-momentum, y-momentum, total energy
+   'flux-exprs-x (list
+                  `mom_x
+                  `(+ (/ (* mom_x mom_x) rho) (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho))))))
+                  `(* mom_y (/ mom_x rho))
+                  `(* (+ energy (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) (/ mom_x rho)))
+                                                         ; flux vector (x-direction)
+   'flux-exprs-y (list
+                  `mom_y
+                  `(* mom_x (/ mom_y rho))
+                  `(+ (/ (* mom_y mom_y) rho) (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho))))))
+                  `(* (+ energy (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) (/ mom_y rho)))
+                                                         ; flux vector (y-direction)
+   'max-speed-exprs-x (list
+                       `(abs (- (/ mom_x rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) rho))))
+                       `(abs (/ mom_x rho))
+                       `(abs (/ mom_x rho))
+                       `(abs (+ (/ mom_x rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) rho)))))
+                                                         ; local wave-speeds (x-direction)
+   'max-speed-exprs-y (list
+                       `(abs (- (/ mom_y rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) rho))))
+                       `(abs (/ mom_y rho))
+                       `(abs (/ mom_y rho))
+                       `(abs (+ (/ mom_y rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (+ (/ (* mom_x mom_x) rho) (/ (* mom_y mom_y) rho)))))) rho)))))
+                                                         ; local wave-speeds (y-direction)
+   'parameters (list
+                `(define gamma 1.4))                     ; adiabatic index: gamma = 1.4
+   ))
+
+;; Define 2D simulation parameters.
+(define nx-2d 50)
+(define ny-2d 50)
+(define x0-2d 0.0)
+(define x1-2d 1.0)
+(define y0-2d 0.0)
+(define y1-2d 1.0)
+(define t-final-2d 0.8)
+(define cfl-2d 0.95)
+(define init-funcs-2d (list
+                       `(cond
+                          [(> y 0.8)
+                           (cond
+                             [(< x 0.8) 0.5323]
+                             [else 1.5])]
+                          [else
+                           (cond
+                             [(< x 0.8) 0.138]
+                             [else 0.5323])])
+                       `(cond
+                          [(> y 0.8)
+                           (cond
+                             [(< x 0.8) 0.641954]
+                             [else 0.0])]
+                          [else
+                           (cond
+                             [(< x 0.8) 0.166428]
+                             [else 0.0])])
+                       `(cond
+                          [(> y 0.8)
+                           (cond
+                             [(< x 0.8) 0.0]
+                             [else 0.0])]
+                          [else
+                           (cond
+                             [(< x 0.8) 0.166428]
+                             [else 0.641954])])
+                       `(cond
+                          [(> y 0.8)
+                           (cond
+                             [(< x 0.8) 1.1371]
+                             [else 3.75])]
+                          [else
+                           (cond
+                             [(< x 0.8) 0.273212]
+                             [else 1.1371])])))
+
+;; Define (shallow) neural network hyperparameters for 2D.
+(define neural-net-shallow-2d
+  (hash
+   'max-trains 10000    ; maximum number of training steps: 10000
+   'width 64            ; number of neurons in each layer: 64
+   'depth 6             ; total number of layers: 8
+   ))
+
+;; Synthesize the code to train a Lax-Friedrichs surrogate solver for the 2D Euler equations using a shallow neural network.
+(define code-euler-lax-train-2d
+  (train-lax-friedrichs-vector4-2d pde-system-euler-2d neural-net-shallow-2d
+                                   #:nx nx-2d
+                                   #:ny ny-2d
+                                   #:x0 x0-2d
+                                   #:x1 x1-2d
+                                   #:y0 y0-2d
+                                   #:y1 y1-2d
+                                   #:t-final t-final-2d
+                                   #:cfl cfl-2d
+                                   #:init-funcs init-funcs-2d))
+
+;; Output the code to a file.
+(with-output-to-file "code/euler_lax_train_2d.c"
+  #:exists 'replace
+  (lambda ()
+    (display code-euler-lax-train-2d)))
