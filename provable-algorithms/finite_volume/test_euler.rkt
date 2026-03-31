42a32497864855c180e267c6f99b44643246046c
JonathanGorard <jg865@cantab.ac.uk>
2026-03-31 00:04:47 -0400

    Initial commit of the prototype code generator for Lax-Friedrichs solvers (first-order only, no flux extrapolation) for 1D coupled matrix PDE systems, as a step towards full formal verification of a compressible hydrodynamics (Euler) solver. Added skeleton of an 1D compressible Euler test. The Sod shock tube problem runs with this code generator (very diffusively), although there is no theorem-proving architecture yet.

---
+++ provable-algorithms/finite_volume/test_euler.rkt
@@ -0,0 +1,68 @@
+#lang racket
+
+(require "code_generator_core.rkt")
+(require "code_generator_matrix.rkt")
+(require "prover_core.rkt")
+(provide (all-from-out "code_generator_core.rkt"))
+(provide (all-from-out "code_generator_matrix.rkt"))
+
+;; Construct /code and /proofs output directories if they do not already exist.
+(cond
+  [(not (directory-exists? "code")) (make-directory "code")])
+(cond
+  [(not (directory-exists? "proofs")) (make-directory "proofs")])
+
+;; Define the 1D Euler equations (density, x-momentum, and total energy components).
+(define pde-system-euler
+  (hash
+   'name "euler"
+   'cons-exprs (list
+                `rho
+                `mom_x
+                `energy)               ; conserved variables: density, x-momentum, total energy
+   'flux-exprs (list
+                `mom_x
+                `(+ (/ (* mom_x mom_x) rho) (* (- gamma 1.0) (- energy (* 0.5 (/ (* mom_x mom_x) rho)))))
+                `(* (+ energy (* (- gamma 1.0) (- energy (* 0.5 (/ (* mom_x mom_x) rho))))) (/ mom_x rho)))
+                                       ; flux vector
+   'max-speed-exprs (list
+                     `(abs (- (/ mom_x rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (/ (* mom_x mom_x) rho))))) rho))))
+                     `(abs (/ mom_x rho))
+                     `(abs (+ (/ mom_x rho) (sqrt (/ (* gamma (* (- gamma 1.0) (- energy (* 0.5 (/ (* mom_x mom_x) rho))))) rho)))))
+                                       ; local wave-speeds
+   'parameters (list
+                `(define gamma 1.4))   ; adiabatic index: gamma = 1.4
+   ))
+
+;; Define simulation parameters.
+(define nx 200)
+(define x0 0.0)
+(define x1 1.0)
+(define t-final 0.2)
+(define cfl 0.95)
+(define init-funcs (list
+                    `(cond
+                       [(< x 0.5) 3.0]
+                       [else 1.0])
+                    `(cond
+                       [(< x 0.5) 0.0]
+                       [else 0.0])
+                    `(cond
+                       [(< x 0.5) 7.5]
+                       [else 2.5])))
+
+;; Synthesize the code for a Lax-Friedrichs solver for the 1D Euler equations (density, x-momentum, and total energy components).
+(define code-euler-lax
+  (generate-lax-friedrichs-vector3-1d pde-system-euler
+                                      #:nx nx
+                                      #:x0 x0
+                                      #:x1 x1
+                                      #:t-final t-final
+                                      #:cfl cfl
+                                      #:init-funcs init-funcs))
+
+;; Output the code to a file.
+(with-output-to-file "code/euler_lax.c"
+  #:exists 'replace
+  (lambda ()
+    (display code-euler-lax)))
